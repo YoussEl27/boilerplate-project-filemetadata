924380849b0b9f032778601598667247086b87e6
elkettani <youssef.elkettani@stud.th-luebeck.de>
2025-09-28 00:29:28 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 var app = express();
 
@@ -12,9 +17,20 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const file = req.file
+  if (!file) return res.json({error: 'File doesn\'t exist'});
+
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  })
+})
 
 
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
